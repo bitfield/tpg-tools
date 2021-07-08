7afff1ddb35c31069af25d2891b07d3e1fc1b7c8
John Arundel <john@bitfieldconsulting.com>
2021-07-08 15:22:43 +0100

    Add Listing 5.3

---
+++ 5.3/count.go
@@ -0,0 +1,98 @@
+package count
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+type counter struct{
+	wordCount bool
+	input io.Reader
+	output io.Writer
+}
+
+type option func(*counter) error
+
+func WithInput(input io.Reader) option {
+	return func (c *counter) error {
+		if input == nil {
+			return errors.New("nil input reader")
+		}
+		c.input = input
+		return nil
+	}
+}
+
+func WithArgs(args []string) option {
+	return func (c *counter) error {
+		fs := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+		wordCount := fs.Bool("w", false, "Count words instead of lines")
+		err := fs.Parse(args)
+		if err != nil {
+			return err
+		}
+		c.wordCount = *wordCount
+		args = fs.Args()
+		if len(args) < 1 {
+			return errors.New("no args supplied")
+		}
+		f, err := os.Open(args[0])
+		if err != nil {
+			return err
+		}
+		c.input = f
+		return nil
+	}
+}
+
+func WithOutput(output io.Writer) option {
+	return func (c *counter) error {
+		if output == nil {
+			return errors.New("nil output writer")
+		}
+		c.output = output
+		return nil
+	}
+}
+
+func NewCounter(opts ...option) (counter, error) {
+	c := counter{
+		input: os.Stdin,
+		output: os.Stdout,
+	}
+	for _, opt := range opts {
+		err := opt(&c)
+		if err != nil {
+			return counter{}, err
+		}
+	}
+	return c, nil
+}
+
+func (c counter) Lines() {
+	fmt.Fprintln(c.output, c.Count())
+}
+
+func (c counter) Count() int {
+	count := 0
+	scanner := bufio.NewScanner(c.input)
+	if c.wordCount {
+		scanner.Split(bufio.ScanWords)
+	}
+	for scanner.Scan() {
+		count++
+	}
+	return count
+}
+
+func Lines() {
+	c, err := NewCounter()
+	if err != nil {
+		panic("internal error calling NewCounter")
+	}
+	c.Lines()
+}
---
+++ 5.3/count_test.go
@@ -0,0 +1,85 @@
+package count_test
+
+import (
+	"bytes"
+	"count"
+	"testing"
+)
+
+func TestCountLines(t *testing.T) {
+	t.Parallel()
+	outputBuf := &bytes.Buffer{}
+	inputBuf := bytes.NewBufferString("1\n2\n3")
+	c, err := count.NewCounter(
+		count.WithInput(inputBuf),
+		count.WithOutput(outputBuf),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "3\n"
+	c.Lines()
+	got := outputBuf.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestCount(t *testing.T) {
+	t.Parallel()
+	inputBuf := bytes.NewBufferString("1\n2\n3")
+	c, err := count.NewCounter(
+		count.WithInput(inputBuf),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 3
+	got := c.Count()
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestWithInputFromArgs(t *testing.T) {
+	t.Parallel()
+	args := []string{"testdata/three_lines.txt"}
+	c, err := count.NewCounter(
+		count.WithArgs(args),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 3
+	got := c.Count()
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestWithInputFromArgsErrorsOnEmptySlice(t *testing.T) {
+	t.Parallel()
+	args := []string{}
+	_, err := count.NewCounter(
+		count.WithArgs(args),
+	)
+	if err == nil {
+		t.Fatal("want error on empty slice, got nil")
+	}
+}
+
+func TestWordCount(t *testing.T) {
+	t.Parallel()
+	args := []string{"-w", "testdata/three_lines.txt"}
+	c, err := count.NewCounter(
+		count.WithArgs(args),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 6
+	got := c.Count()
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
