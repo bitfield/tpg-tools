8a5a1f9eeff1bafba6481caf0c1bb2b08ab1d1db
John Arundel <john@bitfieldconsulting.com>
2021-10-11 16:14:58 +0100

    tweak

--- 8.2/battery.go
+++ 8.2/battery.go
@@ -31,7 +31,7 @@ var pmsetOutput = regexp.MustCompile("([0-9]+)%")
 
 func ParsePmsetOutput(text string) (Status, error) {
 	matches := pmsetOutput.FindStringSubmatch(text)
-	if matches == nil || len(matches) < 2 {
+	if len(matches) < 2 {
 		return Status{}, fmt.Errorf("failed to parse pmset output: %q", text)
 	}
 	charge, err := strconv.Atoi(matches[1])
