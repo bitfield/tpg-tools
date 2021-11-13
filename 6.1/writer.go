bab230b6bfead6cdbe18eb6418dd7d4011c543d5
John Arundel <john@bitfieldconsulting.com>
2021-11-13 15:43:47 +0000

    use mode 0600

--- 6.1/writer.go
+++ 6.1/writer.go
@@ -3,5 +3,9 @@ package writer
 import "os"
 
 func WriteToFile(path string, data []byte) error {
-	return os.WriteFile(path, data, 0666)
+	err := os.WriteFile(path, data, 0600)
+	if err != nil {
+		return err
+	}
+	return os.Chmod(path, 0600)
 }
--- 6.1/writer_test.go
+++ 6.1/writer_test.go
@@ -16,6 +16,14 @@ func TestWriteToFile(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	stat, err := os.Stat(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	perm := stat.Mode().Perm()
+	if perm != 0600 {
+		t.Errorf("want file mode 0600, got 0%o", perm)
+	}
 	got, err := os.ReadFile(path)
 	if err != nil {
 		t.Fatal(err)
@@ -28,7 +36,7 @@ func TestWriteToFile(t *testing.T) {
 func TestWriteToFileClobbers(t *testing.T) {
 	t.Parallel()
 	path := t.TempDir() + "/clobber_test.txt"
-	err := os.WriteFile(path, []byte{4, 5, 6}, 0666)
+	err := os.WriteFile(path, []byte{4, 5, 6}, 0600)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -43,5 +51,27 @@ func TestWriteToFileClobbers(t *testing.T) {
 	}
 	if !cmp.Equal(want, got) {
 		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestPermsClosed(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/perms_test.txt"
+	// Pre-create empty file with open perms
+	err := os.WriteFile(path, []byte{}, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = writer.WriteToFile(path, []byte{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	stat, err := os.Stat(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	perm := stat.Mode().Perm()
+	if perm != 0600 {
+		t.Errorf("want file mode 0600, got 0%o", perm)
 	}
 }
